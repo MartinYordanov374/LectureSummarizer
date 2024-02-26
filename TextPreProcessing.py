6369d80e32ea141977fcf5bb609c26c747bf6899
MartinYordanov374 <martinyordanov284@gmail.com>
2024-02-26 22:32:13 +0200

    Text pre-processing is now in seperate file from summarization

--- TextPreProcessing.py
+++ TextPreProcessing.py
@@ -23,3 +23,5 @@ cleaned_text = [clean_word for clean_word in tokenized_file_content if clean_wor
 lemmatized_text_adverbs = [Lemmatizer.lemmatize(word, pos='a') for word in cleaned_text if str.isalpha(word)]
 lemmatized_text_verbs = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_adverbs if str.isalpha(word)]
 lemmatized_text_nouns = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_verbs if str.isalpha(word)]
+
+
