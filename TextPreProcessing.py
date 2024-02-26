8da13add5279e94976dc0985cf8159bd600fe939
MartinYordanov374 <martinyordanov284@gmail.com>
2024-02-26 23:40:21 +0200

    The sentence order is now preserved after lemmatization

--- TextPreProcessing.py
+++ TextPreProcessing.py
@@ -3,6 +3,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 import contractions
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
 
 Lemmatizer = WordNetLemmatizer()
 def preprocess_data(fileContent):
@@ -11,22 +12,26 @@ def preprocess_data(fileContent):
     # extract sentences
     file_sentences = sent_tokenize(file_content)
     
-    # convert everything to lower case
-    lowercase = [sentence.lower() for sentence in file_sentences]
-
-    # expand contraction words
-    
-    no_contractions_content = contractions.fix(" ".join(lowercase))
-
-    # tokenize data
-    tokenized_file_content = word_tokenize(no_contractions_content)
+    # make sure that the sentences order is shown in the resulting structure
+    sentence_words = [word_tokenize(file_sentence.lower()) for file_sentence in file_sentences]
 
     # get rid of stopwords
-    cleaned_text = [clean_word for clean_word in tokenized_file_content if clean_word not in stopwords.words('english')]
+    clean_sentences = [cleanSentenceWord for sentence in sentence_words for cleanSentenceWord in sentence if cleanSentenceWord not in stopwords.words('english')]
+    
+    cleaned_text = " ".join(clean_sentences).split(' . ')
 
-    # lemmatize words
-    lemmatized_text_adverbs = [Lemmatizer.lemmatize(word, pos='a') for word in cleaned_text if str.isalpha(word)]
-    lemmatized_text_verbs = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_adverbs if str.isalpha(word)]
-    lemmatized_text_nouns = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_verbs if str.isalpha(word)]
+    regex_tokenizer = RegexpTokenizer(r'\w+')
+    punctuation_clean_sentences = [regex_tokenizer.tokenize(sentence) for sentence in cleaned_text]
+    joined_punctuation_clean_sentences = [" ".join(punctuation_clean_sentence) for punctuation_clean_sentence in punctuation_clean_sentences]
+    print(joined_punctuation_clean_sentences[0])
 
-    return " ".join(lemmatized_text_nouns)
+    # lemmatize adjectives
+    
+    lemmatized_text_adjectives = [' '.join([Lemmatizer.lemmatize(word, pos='a') for word in word_tokenize(sentence)]) for sentence in joined_punctuation_clean_sentences]
+    # lemmatize verbs
+
+    lemmatized_text_verbs = [' '.join([Lemmatizer.lemmatize(word, pos='v') for word in word_tokenize(sentence)]) for sentence in lemmatized_text_adjectives]
+    # lemmatize nouns (assuming you meant to lemmatize nouns)
+    lemmatized_text_nouns = [' '.join([Lemmatizer.lemmatize(word, pos='n') for word in word_tokenize(sentence)]) for sentence in lemmatized_text_verbs]
+
+    return lemmatized_text_nouns
