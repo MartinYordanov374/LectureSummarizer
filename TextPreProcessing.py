c5f00da9e76339a685301ef549889d4e6bca10fa
MartinYordanov374 <martinyordanov284@gmail.com>
2024-02-26 22:37:49 +0200

    Text preporcessing file is now a function that returns lemmatized text

--- TextPreProcessing.py
+++ TextPreProcessing.py
@@ -5,23 +5,23 @@ import contractions
 from nltk.stem import WordNetLemmatizer
 
 Lemmatizer = WordNetLemmatizer()
+def preprocess_data(fileContent):
+    # get text input
+    file_content = fileContent
 
-# get text input
-file_content = "This is a sample text that won't be retrieved from the pdf later on."
+    # expand contraction words
 
-# expand contraction words
+    no_contractions_content = contractions.fix(file_content)
 
-no_contractions_content = contractions.fix(file_content)
+    # tokenize data
+    tokenized_file_content = word_tokenize(no_contractions_content)
 
-# tokenize data
-tokenized_file_content = word_tokenize(no_contractions_content)
+    # get rid of stopwords
+    cleaned_text = [clean_word for clean_word in tokenized_file_content if clean_word not in stopwords.words('english')]
 
-# get rid of stopwords
-cleaned_text = [clean_word for clean_word in tokenized_file_content if clean_word not in stopwords.words('english')]
+    # lemmatize words
+    lemmatized_text_adverbs = [Lemmatizer.lemmatize(word, pos='a') for word in cleaned_text if str.isalpha(word)]
+    lemmatized_text_verbs = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_adverbs if str.isalpha(word)]
+    lemmatized_text_nouns = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_verbs if str.isalpha(word)]
 
-# lemmatize words
-lemmatized_text_adverbs = [Lemmatizer.lemmatize(word, pos='a') for word in cleaned_text if str.isalpha(word)]
-lemmatized_text_verbs = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_adverbs if str.isalpha(word)]
-lemmatized_text_nouns = [Lemmatizer.lemmatize(word, pos='v') for word in lemmatized_text_verbs if str.isalpha(word)]
-
-
+    return lemmatized_text_nouns
